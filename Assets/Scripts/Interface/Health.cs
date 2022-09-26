32563a87d0ab8478a182553752e4187e67185df6
LittleLarge <githubandlab@gmail.com>
2022-09-27 03:51:05 +0600

    Second stage of restoring

---
+++ Assets/Scripts/Interface/Health.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    #region UnityMethods
+    private void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+    #endregion
+}
